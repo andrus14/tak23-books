f9a9d379f690e3a0994c21f5d6548db63e1ca378
Andrus <andrus.naulainen@ametikool.ee>
2024-10-28 13:21:38 +0200

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('./connection.php');
+
+$stmt = $pdo->query('SELECT * FROM books');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<ul>
+    <?php while ( $row = $stmt->fetch() ) { ?>
+        
+        <li>
+            <a href="./book.php?id=<?= $row['id']; ?>">
+                <?= $row['title']; ?>
+            </a>
+        </li>
+    
+    <?php } ?>
+</ul>
+
+</body>
+</html>
